365a5d73a6da20bac93a089d50a3e55f05c84140
Su-LQ <suliquangm@gmail.com>
2022-07-12 10:41:04 +0800

    add signup api;

--- webservice/flaskr/wallet.py
+++ webservice/flaskr/wallet.py
@@ -4,14 +4,19 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
+from flask.json import jsonify
 
 from flaskr.db import get_db
+from flaskr.response import Response
 
 bp = Blueprint("wallet", __name__)
 
 @bp.route("/signup", methods = ["GET"])
 def signup():
-    if request.method == "GET":
-        return;
+    resp = Response();
 
-    return;
+    if request.method == "GET":
+        resp.setData({"address":"wallet.address", "private_key":"wallet.private.key"});
+
+
+    return jsonify(str(resp))
