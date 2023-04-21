6e8a47007c87684815ece48d855ce78caba97fd9
huangxingge <huangxingge6280@gmail.com>
2023-04-21 15:14:03 +0800

    update

--- violas/etc/acpi/asus-wireless.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Find and toggle wireless devices on Asus laptops
-
-test -f /usr/share/acpi-support/state-funcs || exit 0
-
-. /usr/share/acpi-support/state-funcs
-
-toggleAllWirelessStates
--- violas/etc/firefox/pref/apturl.js
+++
@@ -1,6 +0,0 @@
-pref("network.protocol-handler.app.apt","/usr/bin/apturl");
-pref("network.protocol-handler.warn-external.apt",false);
-pref("network.protocol-handler.app.apt+http","/usr/bin/apturl");
-pref("network.protocol-handler.warn-external.apt+http",false);
-pref("network.protocol-handler.external.apt",true);
-pref("network.protocol-handler.external.apt+http",true);
